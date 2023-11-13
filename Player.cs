cd71685099ffc8737d25febea23d6dbaa46b52e2
WilliamKSilva <wilz.kelvin12@gmail.com>
2023-11-13 12:28:47 -0300

    feat: add Player Collision handling method and Signal

--- Player.cs
+++ Player.cs
@@ -13,56 +13,75 @@ public partial class Player : Area2D
 		ScreenSize = GetViewportRect().Size;
 	}
 
-    public override void _Process(double delta)
-    {
-			var velocity = Vector2.Zero;
+	public override void _Process(double delta)
+	{
+		var velocity = Vector2.Zero;
 
-			if (Input.IsActionPressed("move_right"))
-			{
-				velocity.X += 1;
-			}
+		if (Input.IsActionPressed("move_right"))
+		{
+			velocity.X += 1;
+		}
 
-			if (Input.IsActionPressed("move_left"))
-			{
-				velocity.X -= 1;
-			}
+		if (Input.IsActionPressed("move_left"))
+		{
+			velocity.X -= 1;
+		}
 
-			if (Input.IsActionPressed("move_down"))
-			{
-				velocity.Y += 1;
-			}
+		if (Input.IsActionPressed("move_down"))
+		{
+			velocity.Y += 1;
+		}
 
-			if (Input.IsActionPressed("move_up"))
-			{
-				velocity.Y -= 1;
-			}
+		if (Input.IsActionPressed("move_up"))
+		{
+			velocity.Y -= 1;
+		}
 
-			var animateSprite2D = GetNode<AnimatedSprite2D>("AnimatedSprite2D");
+		var animateSprite2D = GetNode<AnimatedSprite2D>("AnimatedSprite2D");
 
-			if (velocity.X != 0) {
-				animateSprite2D.Animation = "walk";
-				animateSprite2D.FlipV = false;
-				animateSprite2D.FlipH = velocity.X < 0;
-			}
-			else if (velocity.Y != 0)
-			{
-				animateSprite2D.Animation = "up";
-				animateSprite2D.FlipV = velocity.Y > 0;
-			}
+		if (velocity.X != 0)
+		{
+			animateSprite2D.Animation = "walk";
+			animateSprite2D.FlipV = false;
+			animateSprite2D.FlipH = velocity.X < 0;
+		}
+		else if (velocity.Y != 0)
+		{
+			animateSprite2D.Animation = "up";
+			animateSprite2D.FlipV = velocity.Y > 0;
+		}
 
-			if (velocity.Length() > 0)
-			{
-				velocity = velocity.Normalized() * Speed;
-				animateSprite2D.Play();
-			}
-			else {
-				animateSprite2D.Stop();
-			}
+		if (velocity.Length() > 0)
+		{
+			velocity = velocity.Normalized() * Speed;
+			animateSprite2D.Play();
+		}
+		else
+		{
+			animateSprite2D.Stop();
+		}
 
-			Position += velocity * (float)delta;
-			Position = new Vector2(
-				x: Mathf.Clamp(Position.X, 0, ScreenSize.X),
-				y: Mathf.Clamp(Position.Y, 0, ScreenSize.Y)
-			);
-    }
+		Position += velocity * (float)delta;
+		Position = new Vector2(
+			x: Mathf.Clamp(Position.X, 0, ScreenSize.X),
+			y: Mathf.Clamp(Position.Y, 0, ScreenSize.Y)
+		);
+	}
+
+	public void Start(Vector2 position) {
+		Position = position;
+		Show();
+		GetNode<CollisionShape2D>("CollisionShape2D").Disabled = false;
+	}
+
+	private void OnBodyEntered(Node2D body)
+	{
+		Hide(); // Player disappears after being hit.
+		EmitSignal(SignalName.Hit);
+		// Must be deferred as we can't change physics properties on a physics callback.
+		GetNode<CollisionShape2D>("CollisionShape2D").SetDeferred(CollisionShape2D.PropertyName.Disabled, true);
+	}
+
+	[Signal]
+	public delegate void HitEventHandler();
 }
